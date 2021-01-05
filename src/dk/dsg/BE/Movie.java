a301df9171d1b724b0c0c9511f9243b304175421
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-01-05 10:23:52 +0100

    Update Movie

--- src/dk/dsg/BE/Movie.java
+++ src/dk/dsg/BE/Movie.java
@@ -1,4 +1,52 @@
 package dk.dsg.BE;
 
+import java.util.Date;
+
 public class Movie {
+
+    private final int ID;
+    private String movieName;
+    private final int rating;
+    private String filePath;
+    private Date lastView;
+
+    public Movie(int id, String movieName, int rating, String filePath, Date lastView) {
+        ID = id;
+        this.movieName = movieName;
+        this.rating = rating;
+        this.filePath = filePath;
+        this.lastView = lastView;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public Date getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(Date lastView) {
+        this.lastView = lastView;
+    }
 }
