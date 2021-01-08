77141262a70b72203ee44db6a4dacf18c4803e7d
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-01-08 10:41:40 +0100

    Get categories

--- src/dk/dsg/DAL/CategoryDAO.java
+++ src/dk/dsg/DAL/CategoryDAO.java
@@ -1,5 +1,14 @@
 package dk.dsg.DAL;
 
+import dk.dsg.BE.Category;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDAO {
     private DatabaseDAO databaseConnector;
 
@@ -7,4 +16,22 @@ public class CategoryDAO {
         databaseConnector = new DatabaseDAO();
     }
 
+    public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
+
+        String query = "SELECT * FROM Category";
+        try (Connection connection = databaseConnector.getConnection()) {
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+             while (resultSet.next()) {
+                 categories.add(new Category(
+                         resultSet.getInt("id"),
+                         resultSet.getString("catName")
+                 ));
+             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
 }
