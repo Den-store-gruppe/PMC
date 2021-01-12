d0a05655a6055d6c611494274df87abd2669fab0
lazynormz <skupi1234@gmail.com>
2021-01-12 11:00:55 +0100

    Began the JavaDOC process

--- src/dk/dsg/gui/model/MovieModel.java
+++ src/dk/dsg/gui/model/MovieModel.java
@@ -17,23 +17,45 @@ public class MovieModel {
         movieManager = new MovieManager();
     }
 
+    /***
+     * Puts all the movies from the MovieManager and inserts them into an ObservableList
+     * @return
+     * @throws SQLException
+     * @see MovieManager
+     */
     public ObservableList<Movie> getAllMovies() throws SQLException {
         allMovies = FXCollections.observableArrayList();
         allMovies.addAll(movieManager.getAllMovies());
         return allMovies;
     }
 
+    /***
+     * Adds a movie to the ObservableList
+     * @param movie
+     */
     public void addMovie(Movie movie) {
         movieManager.addMovie(movie);
     }
 
+    /***
+     * Updated the JavaFX TableView to show the new movie,
+     * then calls the DAO to update the DB
+     * @param movie
+     * @throws SQLException
+     * @see dk.dsg.DAL.MovieDAO
+     */
     public void updateMovie(Movie movie) throws SQLException {
         movieManager.updateMovie(movie);
-        //allMovies.add(movie);
         allMovies.clear();
         allMovies.addAll(movieManager.getAllMovies());
     }
 
+
+    /***
+     * Deletes a movie from the DB
+     * @param movie
+     * @see dk.dsg.DAL.MovieDAO
+     */
     public void deleteMovie(Movie movie) {
         movieManager.deleteMovie(movie);
     }
