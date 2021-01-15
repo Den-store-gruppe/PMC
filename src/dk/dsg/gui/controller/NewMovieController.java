dfd3a8302ea1ee394bc59878a11fb1847319ea80
HaraldurJoh <zeasick@gmail.com>
2021-01-15 11:20:43 +0100

    "Movies" Initial directory

--- src/dk/dsg/gui/controller/NewMovieController.java
+++ src/dk/dsg/gui/controller/NewMovieController.java
@@ -56,6 +56,8 @@ public class NewMovieController implements Initializable {
     public void SelectMovieFile(ActionEvent actionEvent) {
         FileChooser fc = new FileChooser();
         fc.setTitle("Select the movie...");
+        fc.setInitialDirectory(new File("Movies/"));
+        fc.setTitle("Select movie");
         fc.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Audio Files", "*.mp4", "*.mpeg4")
         );
